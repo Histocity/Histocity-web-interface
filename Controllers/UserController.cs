9982266a3cf632cc05c1a6c0bc0785ffac75ff39
ElineNube <31698656+ElineNube@users.noreply.github.com>
2021-01-08 15:10:13 +0100

    Delete bevestigingsmail

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -69,8 +69,8 @@ namespace Histocity_Website.Controllers
                     connection.Close();
 
                     //Send Email
-                    SendVerificationLinkEmail(email, user.ActivationCode.ToString());
-                    message = "Registratie is geslaag. Er is een bevestigingsmail gestuurd naar: " + user.EmailID;
+                    //SendVerificationLinkEmail(email, user.ActivationCode.ToString());
+                    message = "Account is aangemaakt. U kunt nu inloggen.";
                     status = true;
                 }
             }
@@ -86,34 +86,34 @@ namespace Histocity_Website.Controllers
 
         }
 
-        [HttpGet]
-        public ActionResult VerifyAccount(string id)
-        {
-            bool status = false;
+        //[HttpGet]
+        //public ActionResult VerifyAccount(string id)
+        //{
+        //    bool status = false;
 
-            try
-            {
-                using (MySqlConnection connection = new MySqlConnection("Database=heroku_9a1fa21f73d10db;Data Source=eu-cdbr-west-03.cleardb.net;User Id=bcfe6ec0812a08;Password=0f9c4546; Pooling=false"))
-                {
-                    connection.Open();
-                    MySqlCommand comm = connection.CreateCommand();
+        //    try
+        //    {
+        //        using (MySqlConnection connection = new MySqlConnection("Database=heroku_9a1fa21f73d10db;Data Source=eu-cdbr-west-03.cleardb.net;User Id=bcfe6ec0812a08;Password=0f9c4546; Pooling=false"))
+        //        {
+        //            connection.Open();
+        //            MySqlCommand comm = connection.CreateCommand();
 
-                    comm.CommandText = "UPDATE users SET IsEmailVerified = true WHERE ActivationCode = @ActivationCode";
-                    comm.Parameters.AddWithValue("@ActivationCode", id);
-                    comm.ExecuteNonQuery();
+        //            comm.CommandText = "UPDATE users SET IsEmailVerified = true WHERE ActivationCode = @ActivationCode";
+        //            comm.Parameters.AddWithValue("@ActivationCode", id);
+        //            comm.ExecuteNonQuery();
 
-                    connection.Close();
-                    status = true;
-                }
-            }
-            catch (Exception e)
-            {
-                ViewBag.Message = "Er is iets misgegaan met de database connectie" + e;
-            }
+        //            connection.Close();
+        //            status = true;
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        ViewBag.Message = "Er is iets misgegaan met de database connectie" + e;
+        //    }
 
-            ViewBag.Status = status;
-            return RedirectToAction("Login", "User");
-        }
+        //    ViewBag.Status = status;
+        //    return RedirectToAction("Login", "User");
+        //}
 
 
         [HttpGet]
@@ -139,11 +139,11 @@ namespace Histocity_Website.Controllers
 
                     while (reader.Read())
                     {
-                        if (reader["IsEmailVerified"].ToString() != "1")
-                        {
-                            ViewBag.Message = "Dit account is nog niet bevestigd";
-                            return View();
-                        }
+                        //if (reader["IsEmailVerified"].ToString() != "1")
+                        //{
+                        //    ViewBag.Message = "Dit account is nog niet bevestigd";
+                        //    return View();
+                        //}
                             if (string.Compare(Crypto.Hash(password), reader["password"].ToString()) == 0)
                             {
                             HttpContext.Session.SetString("userID", reader["UserID"].ToString());
@@ -206,45 +206,45 @@ namespace Histocity_Website.Controllers
             return emailExist;
         }
 
-        [NonAction]
-        public void SendVerificationLinkEmail(string email, string activationCode)
-        {
+        //[NonAction]
+        //public void SendVerificationLinkEmail(string email, string activationCode)
+        //{
 
-            var UriBuilder = new UriBuilder
-            {
-                Scheme = Request.Scheme,
-                Host = "localhost:44374",
-                Path = $"/user/verifyaccount/{activationCode}"
-            };
+        //    var UriBuilder = new UriBuilder
+        //    {
+        //        Scheme = Request.Scheme,
+        //        Host = "localhost:44374",
+        //        Path = $"/user/verifyaccount/{activationCode}"
+        //    };
 
-            var link = UriBuilder.ToString().Replace("[", "").Replace("]","");
+        //    var link = UriBuilder.ToString().Replace("[", "").Replace("]","");
 
-            var fromEmail = new MailAddress("postmaster@sandbox3d882ccfbdc24aac9e1e604029f66c55.mailgun.org", "Histocity");
-            var toEmail = new MailAddress(email);
-            var fromEmailPassword = " e8c4648ea03e80a5fd145a6d4fc24b4d-3d0809fb-945b0d21";
-            string subject = "Je Histocity-account is succesvol aangemaakt";
+        //    var fromEmail = new MailAddress("praktijkopdracht.histocity@gmail.com", "Histocity");
+        //    var toEmail = new MailAddress(email);
+        //    var fromEmailPassword = "Unity2020";
+        //    string subject = "Je Histocity-account is succesvol aangemaakt";
 
-            string body = "<br/><br/>Uw Histocity docenten-account is succesvol aangemaakt. <br>" +
-            "Klik op onderstaande link om uw account te bevestigen:" +
-            " <br/><br/><a href='" + link + "'>" + link + "</a> ";
+        //    string body = "<br/><br/>Uw Histocity docenten-account is succesvol aangemaakt. <br>" +
+        //    "Klik op onderstaande link om uw account te bevestigen:" +
+        //    " <br/><br/><a href='" + link + "'>" + link + "</a> ";
 
-            var smtp = new SmtpClient
-            {
-                Host = "smtp.mailgun.org",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
-            };
+        //    var smtp = new SmtpClient
+        //    {
+        //        Host = "smtp.gmail.com",
+        //        Port = 587,
+        //        EnableSsl = true,
+        //        DeliveryMethod = SmtpDeliveryMethod.Network,
+        //        UseDefaultCredentials = false,
+        //        Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+        //    };
 
-            using (var message = new MailMessage(fromEmail, toEmail)
-            {
-                Subject = subject,
-                Body = body,
-                IsBodyHtml = true
-            })
-                smtp.Send(message);
-        }
+        //    using (var message = new MailMessage(fromEmail, toEmail)
+        //    {
+        //        Subject = subject,
+        //        Body = body,
+        //        IsBodyHtml = true
+        //    })
+        //        smtp.Send(message);
+        //}
     }
 }
