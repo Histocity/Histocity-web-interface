6f2c951574663473ddb74024756374f428e0f9ef
ElineNube <31698656+ElineNube@users.noreply.github.com>
2021-01-08 16:04:39 +0100

    Add student registration

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Histocity_Website.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult List()
+        {
+            if (HttpContext.Session.GetString("userID") == null)
+            {
+                ViewBag.IsLoggedIn = false;
+                return RedirectToAction("Login", "User");
+            }
+            else
+            {
+                ViewBag.IsLoggedIn = true;
+                ViewBag.TeacherID = HttpContext.Session.GetString("userID");
+                return View();
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult Add(string teacherID)
+        {
+            return View();
+        }
+    }
+}
